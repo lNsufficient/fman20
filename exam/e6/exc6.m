ed24633b56f3cb566e8bb7b888dcf77509a15147
lNsufficient <edvard.jo@gmail.com>
2016-10-25 09:32:54 +0200

    e6 A almost done

--- exam/e6/exc6.m
+++ exam/e6/exc6.m
@@ -3,13 +3,122 @@ load('tentadata2016okt.mat')
 
 I = im;
 
+normEd = @(u) sqrt(sum(sum(u.*u)));
+
 x = 141;
 y = 22;
 s = 6;
 I_small = I(x-s:x+s, y-s:y+s);
+I_small_norm = normEd(I_small);
+strI_small = sprintf('I\\_small, norm: %f', I_small_norm);
 
 figure(1)
+image(I)
 imagesc(I);
+%colormap('gray')
 
 figure(2)
+image(I_small);
 imagesc(I_small);
+%colormap('gray')
+
+[up6, up6n, up6x, ~] = projectOnBasis(I_small,basis1to6);
+[up7, up7n, up7x, ~] = projectOnBasis(I_small,basis7);
+
+figure(3)
+for i = 1:6
+    subplot(2,4,i)
+    imagesc(basis1to6(:,:,i))
+    str = sprintf('basis%d',i);
+    title(str)
+end
+subplot(2,4,7)
+imagesc(I_small);
+title(strI_small)
+
+subplot(2,4,8)
+image(up6)
+%imagesc(I_small)
+imagesc(up6, [min(min(I_small)), max(max(I_small))]);
+str = sprintf('up6, norm: %f', up6n);
+title(str);
+
+figure(4)
+subplot(2, 2, 1)
+imagesc(basis7);
+title('basis7');
+
+subplot(2,2,2)
+imagesc(I_small);
+title(strI_small);
+
+subplot(2,2,3)
+imagesc(up7, [min(min(I_small)), max(max(I_small))]);
+%det funkade inte bra att använda samma imagescale på up7 som I_small.
+%Då I_small projicerades på basis7 fick den mycket mindre max- och
+%min-intensitet. 
+imagesc(up7) %Denna ser ut som basis7, eftersom den endast består av basis7 
+%multiplicerat med en skalär. 
+str = sprintf('up7, norm: %f', up7n);
+title(str);
+
+subplot(2,2,4)
+up67 = up6+up7;
+imagesc(up67, [min(min(I_small)), max(max(I_small))]);
+%Genom att använda samma imagesc som på I_small kan jag se om färgerna
+%matchar
+up67n = sqrt(up6n^2+up7n^2);
+str = sprintf('up7+up6, norm: %f', up67n);
+title(str)
+
+TOL = 1e-12;
+%Testing norms
+up67n2 = normEd(up67);
+if abs(up67n2 - up67n) > TOL
+    disp('up67n wrong')
+end
+
+if abs(up6n - normEd(up6)) > TOL
+    disp('up6n wrong')
+end
+
+if abs(up7n ~= normEd(up7)) > TOL
+    disp('up7n wrong')
+end
+
+%calculate |u3|:
+%Eftersom vi har 169 basvektorer och dimensionen av u är 169 så kommer vi
+%kunna avbilda u perfekt med hjälp av samtliga basvektorer (förutsatt att
+%de är ortogonala). Detta ger oss två sätt att beräkna u:
+u3 = I_small - up7 - up6;
+u3n = normEd(u3);
+u3n2 = sqrt(I_small_norm^2 - up6n^2 - up7n^2);
+%Båda gav samma resultat: 36.0453
+
+u1n = up6n
+u2n = up7n
+u3n
+
+figure(5)
+subplot(3,2,1)
+imagesc(I_small)
+title(strI_small)
+
+subplot(3,2,2)
+imagesc(u3+up6+up7)
+title('summan av u1, u2, u3')
+
+subplot(3,2,3)
+imagesc(up6)
+str = sprintf('u1, norm: %f', u1n)
+title(str)
+
+subplot(3,2,4)
+imagesc(up7)
+str = sprintf('u2, norm: %f', u2n)
+title(str)
+
+subplot(3,2,5)
+imagesc(u3)
+str = sprintf('u3, norm: %f', u3n)
+title(str)
--- exam/e6/projectOnBasis.m
+++ exam/e6/projectOnBasis.m
@@ -1,18 +1,37 @@
-function [up, r] = projectOnBasis(u, e1, e2, e3, e4)
-%PROJECTONBASIS Projects an image u onto a basis (e1, e2, e3, e4), result
-%is up and the error norm r = |u-up|.
+function [up, upn, x, r] = projectOnBasis(u, e)
+%PROJECTONBASIS Projects an image u onto a basis spanned by e.
+%   up - orthogonal projection
+%   e - basis, i:th basis vector is found in e(:,:,i).
+%   r = |u-up|.
+%   upn = |up|
 TOL = 1e-9;
 dot = @(x,y) x(:)'*y(:);
-dotfast = @(x,y) sum(sum(conj(x).*y));
+%dotfast = @(x,y) sum(sum(conj(x).*y));
 nom = @(u) sqrt(dot(u,u));
 
-x = [dot(u, e1), dot(u,e2), dot(u,e3), dot(u,e4)];
-xfast = [dotfast(u, e1), dotfast(u,e2), dotfast(u,e3), dotfast(u,e4)];
+[~, ~, o] = size(e);
 
-if sum(abs(x-xfast)./nom(x) > TOL)
-    disp('something is wrong with at least one dot product');
+x = zeros(o,1);
+
+for i = 1:o
+    x(i) = dot(u, e(:,:,i));
 end
 
-up = x(1)*e1 + x(2)*e2 + x(3)*e3 + x(4)*e4;
+%xfast = [dotfast(u, e1), dotfast(u,e2), dotfast(u,e3), dotfast(u,e4)];
+
+% if sum(abs(x-xfast)./nom(x) > TOL)
+%     disp('something is wrong with at least one dot product');
+% end
+
+up = zeros(size(u));
+for i = 1:o
+    up = up + x(i)*e(:,:,i);
+end
+
 r = nom(u - up);
+upn2 = nom(up);
+upn = sqrt(sum(x.^2)); %This requires e to be orthonormal basis
+if abs(upn - upn2) > 1e-11
+    disp('something wrong with norms')
+end
 end
